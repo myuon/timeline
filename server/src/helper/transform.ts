44e9565c932db4ed8ec91d549559d0c903d07aa2
myuon <ioi.joi.koi.loi@gmail.com>
2023-01-10 05:07:22 +0900

    fix: sophisticated escaped string

--- server/src/helper/transform.ts
+++ server/src/helper/transform.ts
@@ -1,11 +1,35 @@
 import escape from "escape-html";
 
 export const transformContent = (content: string) => {
-  return `<p>${escape(content)
-    .replace(
-      /(https?:\/\/[^\s"]+)/,
-      '<a href="$1" rel="noopener noreferer" target="_blank">$1</a>'
-    )
-    .split("\n")
-    .reduce((x, y) => x + "<br />" + y)}</p>`;
+  const tree = [];
+  const urlMatched = content.matchAll(/(https?:\/\/[^\s"]+)/g);
+
+  let prev = 0;
+  for (const match of urlMatched) {
+    if (match.index) {
+      tree.push({ text: content.slice(prev, match.index) });
+      prev = match.index + match[0].length;
+
+      tree.push({
+        url: match[0],
+      });
+    }
+  }
+  tree.push({
+    text: content.slice(prev),
+  });
+
+  let escaped = "";
+  for (const node of tree) {
+    if (node.text) {
+      escaped += escape(node.text);
+    }
+    if (node.url) {
+      escaped += `<a href="${
+        node.url
+      }" rel="noopener noreferer" target="_blank">${escape(node.url)}</a>`;
+    }
+  }
+
+  return `<p>${escaped.split("\n").reduce((x, y) => x + "<br />" + y)}</p>`;
 };
--- server/test/helper/transform.spec.ts
+++ server/test/helper/transform.spec.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { transformContent } from "../../src/helper/transform";
 
 describe("transformContent", () => {
-  it("should parse URL", () => {
+  it("should parse a URL", () => {
     const actual = transformContent(`See: https://example.com`);
     assert.equal(
       `<p>See: <a href="https://example.com" rel="noopener noreferer" target="_blank">https://example.com</a></p>`,
@@ -13,7 +13,15 @@ describe("transformContent", () => {
   it("should parse query string", () => {
     const actual = transformContent(`See: https://example.com?foo=bar&baz=10`);
     assert.equal(
-      `<p>See: <a href="https://example.com?foo=bar&baz=10" rel="noopener noreferer" target="_blank">https://example.com?foo=bar&baz=10</a></p>`,
+      `<p>See: <a href="https://example.com?foo=bar&baz=10" rel="noopener noreferer" target="_blank">https://example.com?foo=bar&amp;baz=10</a></p>`,
+      actual
+    );
+  });
+
+  it("should parse a URL enclosed by quote", () => {
+    const actual = transformContent(`See: "https://example.com"`);
+    assert.equal(
+      `<p>See: &quot;<a href="https://example.com" rel="noopener noreferer" target="_blank">https://example.com</a>&quot; </p>`,
       actual
     );
   });
