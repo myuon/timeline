02ccda657afb77b5ac2693dc67b0ced9eb654f5f
myuon <ioi.joi.koi.loi@gmail.com>
2023-01-10 04:49:06 +0900

    test: add a test for transformContent

--- server/test/helper/transform.spec.ts
+++ server/test/helper/transform.spec.ts
@@ -1,10 +1,20 @@
 import assert from "assert";
 import { transformContent } from "../../src/helper/transform";
 
-it("transformContent", () => {
-  const actual = transformContent(`See: https://example.com`);
-  assert.equal(
-    `<p>See: <a href="https://example.com" rel="noopener noreferer" target="_blank">https://example.com</a></p>`,
-    actual
-  );
+describe("transformContent", () => {
+  it("should parse URL", () => {
+    const actual = transformContent(`See: https://example.com`);
+    assert.equal(
+      `<p>See: <a href="https://example.com" rel="noopener noreferer" target="_blank">https://example.com</a></p>`,
+      actual
+    );
+  });
+
+  it("should parse query string", () => {
+    const actual = transformContent(`See: https://example.com?foo=bar&baz=10`);
+    assert.equal(
+      `<p>See: <a href="https://example.com?foo=bar&baz=10" rel="noopener noreferer" target="_blank">https://example.com?foo=bar&baz=10</a></p>`,
+      actual
+    );
+  });
 });
