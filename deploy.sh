8ad5369039e691b104f2d1a2f95a37fcc65110bc
myuon <ioi.joi.koi.loi@gmail.com>
2023-01-21 16:42:33 +0900

    fix: delete images

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ docker build . --tag $IMAGE_PATH
 docker push $IMAGE_PATH
 gcloud run deploy timeline --image $IMAGE_PATH --region asia-northeast1
 
-digests=$(gcloud container images list-tags $IMAGE_NAME --sort-by=~TIMESTAMP --limit=5 --format='get(digest)')
+digests=$(gcloud container images list-tags $IMAGE_PATH --sort-by=~TIMESTAMP --format='get(digest)')
+digests_to_delete=$(echo "$digests" | tail -n +6)
 
-for digest in $digests; do
-  if [[ $digest != $(echo "$digests" | tail -n 1) ]]; then
-    gcloud container images delete $IMAGE_NAME@$digest --force-delete-tags
-  fi
+for digest in $digests_to_delete; do
+  echo $IMAGE_NAME@$digest
+  gcloud container images delete $IMAGE_PATH@$digest --force-delete-tags --quiet
 done
